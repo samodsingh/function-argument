1800e3d462f333b5be561341bfb699f907b3eba6
samod singh <samod0098@gmail.com>
2021-03-10 13:40:28 +0530

    function argument

---
+++ my.js
@@ -0,0 +1,16 @@
+    //function argument 
+// function add(a, b, c)
+// {
+//         document.write("A:" + a  + "B: " + b + "C:" + c); 
+
+// }
+// add( 10, 20,30);
+
+//function argument missing
+
+function add(a, b, c)
+{
+        document.write("A:" + a  + "B: " + b + "C:" + c); 
+
+}
+add( 10, 20);
